8da181f40b6818a9db5de58bcb3f3f1e1d4aaaf0
WilliamCarlos <masteratsoccer@gmail.com>
2018-02-21 16:10:24 -0500

    alg/geometric multiplicities

--- assignment2.py
+++ assignment2.py
@@ -3,32 +3,32 @@ from sympy import Matrix
 import pdb # todo remove
 
 def create_random_5x5():
-    return np.random.random_sample((5,5))
+    # return np.random.random_sample((5,5))
     return np.random.randint(10, size=(5,5)) # todo what's a good range?
     
         
 def main():
     # create 100 random matrices
     matrix_list = [create_random_5x5() for i in range(100)]
-    get_info(matrix_list[0])
+    display_info(matrix_list[0])
     
     pdb.set_trace()
 
-def get_info(A):
+def display_info(A):
     evals = eigenvalues(A)
+
+    evals, evecs = np.linalg.eig(A)
+
     print('There are {} distinct evals'.format(len(set(evals))))
     print('The largest eval is {}'.format(max(evals)))
     print('The largest eval is {}'.format(min(evals)))
 
-    print('The algebraic multiplicitiy: {}'.format(''' TODO '''))
-    print('The geometric multiplicitiy: {}'.format(''' TODO '''))
-    print('And hence, the geometric multiplicitiy: {}'.format(''' TODO '''))
+    num_indep_evals = len(set([str(x) for x in evals]))
+    print('The algebraic multiplicitiy: {}'.format(num_indep_evals)) 
+    num_indep_evecs = len(set([''.join(str(list(evecs[:,i]))) for i in range(evecs.shape[1])]))    
+    print('The geometric multiplicitiy: {}'.format(num_indep_evecs))
 
-    print('Finally, the matrix in jordon normal form: ')
 
-    sympy_matrix = Matrix(A)
-    P, J = sympy_matrix.jordan_form()
-    print(J)
     import pdb
     pdb.set_trace()
 
