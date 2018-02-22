c399f77353f585abb9ce2b85e54334dd96f8897a
WilliamCarlos <masteratsoccer@gmail.com>
2018-02-21 23:14:58 -0500

    added comments

--- README.md
+++ README.md
@@ -4,12 +4,30 @@
 ### Info Dictionary
 The info dictionary has 4 keys.
 
-* __max__: stores the maximum eigenvalue from our list of eigenvalues. We use the default Python (max) function to get this, which supposedly only looks at the Real part of a complex number.    
+* info[__max__]: stores the maximum eigenvalue from our list of eigenvalues. We use the default Python (max) function to get this, which supposedly only looks at the Real part of a complex number.    
 
-* __min__: Ditto, but with min()   
+* info[__min__]: Ditto, but with min()   
 
-* __Default Dictionary__: This is not a key in our info dictionary, but the next two keys (algebraic_multiplicities and associated_evecs) are objects of these types. A default dictionary is a like a regular python dictionary, but every key in the dictionary is initialized to some value that you specify. We use defaultdict(list) and defaultdict(int), which initialize every bucket in the dictionary with a list and (int) 0 respectively. This allows you to do things like someintdefaultdict[randomkey] += 1, or somelistdefaultdict[randomkey].append(25) without having to check if the key is defined and initialize something there if it isn't. Very useful.    
+* info[__algebraic_multiplicities__]: a dictionary that maps each eigenvalue to the number of times (int) we've seen that eigenvalue. In all the matrices we've tried so far, this has been 1 (unique eigenvalues). This corresponds to the algebraic multiplicity of the eigenvalue.    
 
-* __algebraic_multiplicities__: This is a defaultdict(int) that maps each eigenvalue to the number of times we've seen that eigenvalue. In all the matrices we've tried so far, this has been 1 (unique eigenvalues). This corresponds to the algebraic multiplicity of the eigenvalue.    
+* info[__associated_evecs__]: a dictionary that maps each eigenvalue to a list of corresponding eigenvectors. In all the matrices we've tried so far, the list has len()==1. This means in all the matrices we've tested so far, each eigenvalue corresponds to a unique eigenvector. The length of this list (the number of corresponding eigenvectors for this eigenvalue) corresponds to the geometric multiplicity of the eigenvalue.
 
-* __associated_evecs__: This is a defaultdict(list) that maps each eigenvalue to a list of corresponding eigenvectors. In all the matrices we've tried so far, the list has len()==1. This means in all the matrices we've tested so far, each eigenvalue corresponds to a unique eigenvector. This value also corresponds to the geometric multiplicity of the eigenvalue. 
+* __Default Dictionary__: This is not a key in our info dictionary, but we used it to compute the previous two keys (algebraic_multiplicities and associated_evecs). A default dictionary is a like a regular python dictionary, but every key in the dictionary is initialized to some value that you specify. We use defaultdict(list) and defaultdict(int), which initialize every bucket in the dictionary with a list and (int) 0 respectively. This allows you to do things like someintdefaultdict[randomkey] += 1, or somelistdefaultdict[randomkey].append(25) without having to check if the key is defined and initialize something there if it isn't. Very useful.    
+
+
+
+MatPlotLib Code for Later Ref
+```
+plt.title("Distribution of maximum eigenvalues with {} matrices".format(n))
+plt.xlabel('Eigenvalue')
+plt.ylabel('Frequency')
+plt.hist(max_eval_list, normed=False, bins=30)
+plt.show()
+
+plt.title("Distribution of minimum eigenvalues with {} matrices".format(n))
+plt.xlabel('Eigenvalue')
+plt.ylabel('Frequency')
+plt.hist(min_eval_list, normed=False, bins=30)
+plt.show()
+```
+
--- assignment2.py
+++ assignment2.py
@@ -25,36 +25,28 @@ def main():
         # Read more about each of the keys in the readme
         # GoTo: https://github.com/WilliamCarlos/topics_in_algebra
         info = get_info(matrix)
-        max_eval_list.append(info['max'])
-        min_eval_list.append(info['min'])
+
+        # For each e_val we have
         for e_val in info['algebraic_multiplicities']:
             geometric_multiplicity = len(info['associated_evecs'][e_val])
+            # check if the alg-multiplicity equals geo-multiplicity
             if info['algebraic_multiplicities'][e_val] != geometric_multiplicity:
                 print('The {}th matrix is not diagonalizable! This is a first.'.format(i))
                 pdb.set_trace()
-
+            # check if our e_vals or e_vecs are non_unique. AKA non-one alg/geo multiplicity
             if info['algebraic_multiplicities'][e_val] > 1 or  geometric_multiplicity > 1:
+                print('Greater than 1 alg/geo multiplicity! This is a first')
                 pdb.set_trace()
 
+    # If we haven't triggered the two if statements above, we know all the matrices
+    # We have tried so far have been diagonalizable. Further, the alg/geo multiplicity
+    # for all the e_vals in the matrices is 1. 
     print('\n****************************************************************************'\
           '\nAll {} randomly created matrices are diagonalizable (algebraic multiplicity' \
           ' is equal to the geometric multiplicity for all eigenvalues). \n' \
           '****************************************************************************'.format(n))
 
-    ''' Plot Graphs
-    plt.title("Distribution of maximum eigenvalues with {} matrices".format(n))
-    plt.xlabel('Eigenvalue')
-    plt.ylabel('Frequency')
-    plt.hist(max_eval_list, normed=False, bins=30)
-    plt.show()
-
-    plt.title("Distribution of minimum eigenvalues with {} matrices".format(n))
-    plt.xlabel('Eigenvalue')
-    plt.ylabel('Frequency')
-    plt.hist(min_eval_list, normed=False, bins=30)
-    plt.show()
-    '''
-    
+    # User Interaction Loop. Option to display more info about any matrix in our matrix_list.
     while True:
         more_info = input('\nThere are {} random matrices. Type a number in range [0,{}] '\
                           "to display more info about that matrix. Enter 'exit' to exit \n".format(n,n-1))
@@ -63,20 +55,29 @@ def main():
         display_info(matrix_list[int(more_info)])        
     
 def get_info(A):
+    ''' Returns info dictionary w/ 4 keys. Keys described in readme.md
+    '''
+    # get evals, evecs of the matrix numpy numpy
+    # evals is a column vector. evecs is a 5x5 matrix, with each column being an evec.
+    # So the ith eval is evals[i] and the corresponding evec for that eval is evecs[:i]
+    # Numpy indexing is always matrix[row][col]
     evals, evecs = np.linalg.eig(A)    
     
     info = {}
-    info['max'] = max(evals) # why discrete? 
+    info['max'] = max(evals) 
     info['min'] = min(evals)
 
-    # maps eval to frequency
-    algebraic_multiplicities = defaultdict(int)
-    # maps eval to associated evecs
+    # initialize dictionary that will map eval to # times we have seen that eval
+    algebraic_multiplicities = defaultdict(int)    
+    # initialize dictionary that will map eval a list of corresponding evecs
     associated_evecs = defaultdict(list)
-    
+
+    # populate both dictionaries
     for e_val in evals:
         algebraic_multiplicities[e_val] += 1
         associated_evecs[e_val].append(e_val)
+
+    # type-cast back into regular dictionaries before adding to info dictionary
     info['algebraic_multiplicities'] = dict(algebraic_multiplicities)
     info['associated_evecs'] = dict(associated_evecs)
                
@@ -100,3 +101,6 @@ def display_info(A):
 
 if __name__ == '__main__':
     main()
+
+
+
