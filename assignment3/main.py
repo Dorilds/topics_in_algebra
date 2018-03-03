a8827afd1079d5aea0e5e2fd22c39434890c9e4a
WilliamCarlos <masteratsoccer@gmail.com>
2018-03-03 13:42:45 -0500

    finished initialize_transition_matrix()

---
+++ assignment3/main.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pdb # todo remove
+
+class Frog:
+    def __init__(self, frog_number):
+        self.frog_number = frog_number
+        
+class Lake:
+    def __init__(self, num_pads, num_frogs):
+        self.pads = self.initialize_pads_dict(num_pads, num_frogs)
+        self.transition_matrix = self.initialize_transition_matrix(num_pads)
+        pdb.set_trace()
+
+    def initialize_transition_matrix(self, num_pads):
+        ''' Initialize nxn transition matrix '''
+        transition_matrix = np.zeros((num_pads, num_pads))
+        for row_idx in range(num_pads):
+            transition_matrix[row_idx][row_idx-1] = 1/3
+            transition_matrix[row_idx][row_idx] = 1/3
+            transition_matrix[row_idx][(row_idx+1) % num_pads] = 1/3 # %num_pads for wraparound
+        return transition_matrix
+    def initialize_pads_dict(self, num_pads, num_frogs):
+        return
+        
+def main():
+    np.set_printoptions(precision=1)
+    num_pads = 12
+    num_frogs = 100
+    lake = Lake(num_pads, num_frogs)
+    
+if __name__ == "__main__":
+    main()
