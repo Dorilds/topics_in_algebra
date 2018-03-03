4216ff67f1c613de1c84af49004ef6d1e0abc499
WilliamCarlos <masteratsoccer@gmail.com>
2018-03-03 16:18:34 -0500

    debug and output graphs

--- assignment3/q4.py
+++ assignment3/q4.py
@@ -1,4 +1,6 @@
 import numpy as np
+import os
+import matplotlib.pyplot as plt
 
 def initialize_transition_matrix(num_pads):
     ''' Initialize nxn transition matrix '''
@@ -19,15 +21,13 @@ def write_eval_dict(d):
     f = open('q4/2nd_max_evals.txt', 'w')
     f.write('N and 2nd highest eval')
     for key, val in d.items():
-        f.write('{N:} {}'.format(key, val))
+        f.write('N:{} {}'.format(key, val))
         
 def plot_2nd_evals(d):
-    if not os.path.exists('q4'):
-         os.makedirs('q4')
-         
     plt.title('2nd Highest Eval vs N')
     plt.xlabel('N')
     plt.ylabel('Eval Value')
+    plt.ylim((0,1))
     plt.bar(range(len(d)), d.values(), align='center')
     plt.xticks(range(len(d)), d.keys())
     plt.savefig('q4/2nd_max_evals.png', bbox_inches='tight')
@@ -38,22 +38,25 @@ def get_evals(A):
     return e_vals
     
 def main():
-    2nd_max_evals = {}
-    for n in range(3, 30):
+
+    second_max_evals = {}
+    for n in range(20):
         A = initialize_transition_matrix(n)
-        e_vals = sorted(self.get_evals(self.transition_matrix), reverse=True)
+        e_vals = sorted(get_evals(A), reverse=True)
         if len(e_vals) < 2:
             print("MISSED ONE")
             continue
-        2nd_max_eval = e_vals[1]
-        print(2nd_max_eval)
+        second_max_eval = e_vals[1]
+        print(second_max_eval)
+        second_max_evals[n] = second_max_eval
 
-    write_eval_dict(2nd_max_evals)
-    plot_2nd_evals(2nd_max_evals)
+    if not os.path.exists('q4'):
+        os.makedirs('q4')
+        
+    write_eval_dict(second_max_evals)
+    plot_2nd_evals(second_max_evals)
 
-'''
+    
 if __name__ == "__main__":
-    test()
     main()
 
-'''
