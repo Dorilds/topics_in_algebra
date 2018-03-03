c7d5d967d5c8b1311f5775de35693cccb678dabf
WilliamCarlos <masteratsoccer@gmail.com>
2018-03-03 14:14:10 -0500

    finished and tested get_next_pad()

--- assignment3/main.py
+++ assignment3/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 import pdb # todo remove
 
 class Frog:
@@ -9,6 +10,10 @@ class Lake:
     def __init__(self, num_pads, num_frogs):
         self.pads_dict = self.initialize_pads_dict(num_pads, num_frogs)
         self.transition_matrix = self.initialize_transition_matrix(num_pads)
+
+        from collections import defaultdict
+        d = defaultdict(int)
+        
         pdb.set_trace()
 
     def initialize_transition_matrix(self, num_pads):
@@ -21,15 +26,37 @@ class Lake:
         return transition_matrix
     
     def initialize_pads_dict(self, num_pads, num_frogs):
+        ''' Initialize pads dict and populate with frogs all on pad 0'''
         pads = {}
         for i in range(num_pads):
             pads['pad{}'.format(i)] = []
+
+        for i in range(num_frogs):
+            pads['pad0'].append(Frog(i))
         return pads
-        
+
+    def get_next_pad(self, current_pad):
+        ''' Given the index of the current pad, uses transition matrix to return next pad'''
+        row = self.transition_matrix[current_pad]
+        probability_total = np.zeros_like(row)
+        for i in range(len(probability_total)):
+            if i == 0:
+                probability_total[i] = row[i]
+                continue
+            else:
+                probability_total[i] = row[i] + probability_total[i-1]
+        rand_prob = random.random()
+        for i in range(len(probability_total)):
+            if rand_prob < probability_total[i]:
+                return i            
+        pdb.set_trace()
+        raise Exception('disaster')
+    
 def main():
     np.set_printoptions(precision=1)
     num_pads = 12
     num_frogs = 100
+
     lake = Lake(num_pads, num_frogs)
     
 if __name__ == "__main__":
