07278fb3d2fabc0235722fae93ef816c51420e5b
WilliamCarlos <masteratsoccer@gmail.com>
2018-03-03 13:45:10 -0500

    finished initialize_pads_dict()

--- assignment3/main.py
+++ assignment3/main.py
@@ -7,7 +7,7 @@ class Frog:
         
 class Lake:
     def __init__(self, num_pads, num_frogs):
-        self.pads = self.initialize_pads_dict(num_pads, num_frogs)
+        self.pads_dict = self.initialize_pads_dict(num_pads, num_frogs)
         self.transition_matrix = self.initialize_transition_matrix(num_pads)
         pdb.set_trace()
 
@@ -19,8 +19,12 @@ class Lake:
             transition_matrix[row_idx][row_idx] = 1/3
             transition_matrix[row_idx][(row_idx+1) % num_pads] = 1/3 # %num_pads for wraparound
         return transition_matrix
+    
     def initialize_pads_dict(self, num_pads, num_frogs):
-        return
+        pads = {}
+        for i in range(num_pads):
+            pads['pad{}'.format(i)] = []
+        return pads
         
 def main():
     np.set_printoptions(precision=1)
